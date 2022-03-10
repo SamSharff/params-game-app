a007dfdd851c6e0016e4782e2185a599b892339d
Sam Sharff <samanthaleec@gmail.com>
2022-03-10 17:05:00 -0500

    adds exercises from params day.

--- app/controllers/name_controller.rb
+++ app/controllers/name_controller.rb
@@ -1,6 +1,26 @@
 class NameController < ApplicationController
   def get_name
-  input_name = params["your_name"]
-  render json: {message: "Your name is #{input_name}"} 
+    input_name = params["your_name"]
+    render json: {message: "Your name is #{input_name}"} 
+  end
+
+  def guess_query
+    winning_number = 32
+    input_guess = params["users_guess"].to_i
+    if input_guess > winning_number
+      output_message = "Guess lower!"
+    elsif
+      input_guess < winning_number
+      output_message = "Guess higher!"
+    else
+      output_message = "YOU DID IT. YAY."
+    end
+    render json: {message: output_message}
+  end
+
+  def segment_params
+    input_value = params["this_is_a_variable"]
+    second_value = params["another_variable"]
+    render json: {message: "The url segment is #{input_value} but also #{second_value}"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/name_path", controller: "name", action: "get_name"
+  
+  get "/number_1/:something", controller: "name", action: "get_numbers"
+
+  # post "/guess_body" params examples 
 end
